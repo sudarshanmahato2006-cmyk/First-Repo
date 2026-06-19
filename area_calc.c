42a009db4d7c00348cee78409c758cb03dc462f6
sudarshanmahato2006-cmyk <sudarshanmahato2006@gmail.com>
2026-06-19 21:17:05 +0530

    Add rectangle area calculation program
    This program calculates the area of a rectangle based on user input for length and width.

---
+++ area_calc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int getLength(void) {
+    int length;
+    printf("Enter value: ");
+    scanf("%d", &length);
+    return length;
+}
+
+int getWidth(void) {
+    int width;
+    printf("Enter rectangle width: ");
+    scanf("%d", &width);
+    return width;
+}
+
+int RectangleArea(int length, int width) {
+    int area = length * width;
+    printf("The area of the rectangle is %d\n", area);
+    return area;
+}
+
+
+
+
+
+
+
+int main(void) {
+    int length = getLength();
+    int width = getWidth();
+    RectangleArea(length, width);
+    int a =getLength();
+    printf("%d",a);
+    return 0;
+}
